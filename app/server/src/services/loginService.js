dc1d3f07b51980537199151103e27b5e7659a38e
Burnin <burninandrade@icloud.com>
2020-10-19 18:24:34 -0300

    Login Service Object

--- app/server/src/services/loginService.js
+++ app/server/src/services/loginService.js
@@ -1,19 +1,19 @@
 const connect = require("../connect/connect-client");
 
 exports.signUserWithEmailAndPassword = async (email, password) => {
-  let res = {};
-  const client = await connect.client
+  let login = {};
+  await connect.client
     .auth()
     .signInWithEmailAndPassword(email, password)
-    .catch(
-      err =>
-        (res = {
-          code: err.code,
-          message: err.message,
-        }),
-      res
-    );
-  res["token"] = (await client.user.getIdTokenResult()).token;
-  res["code"]  = res.code  ? res.code : "auth/success"
-  return res;
+    .then(async res => {
+      login["token"] = (await res.user.getIdTokenResult()).token;
+      login["code"] = "auth/success";
+      login["message"] = "Success!";
+    })
+    .catch(err => {
+      login["code"] = err.code;
+      login["message"] = err.message;
+    });
+    console.log(login);
+    return login;
 };
